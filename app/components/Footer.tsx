9538216f6c99a49652ea1b862869c5e6ff897778
SyouHiroki <1114551160@qq.com>
2024-10-18 01:30:03 +0800

    feat: footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,47 @@
-export default function Footer() {
+import Image from "next/image"
+import Link from "next/link"
+
+type LinkType = {text: string, link: string}
+
+export type FooterLinksGroupType = {
+  products: LinkType[],
+  weAlsoBuilt: LinkType[],
+  company: LinkType[]
+}
+
+export default function Footer({text, navsGroup}: {text: string, navsGroup: FooterLinksGroupType}) {
 
   return (
-    <div>
-      Footer
+    <div className="my-10 xl:flex xl:justify-center">
+      <div className="border-t p-2 flex flex-col xl:flex-row xl:gap-9 xl:w-[1280px]">
+        <div className="mt-12 px-2 xl:max-w-[384px]">
+          <Image src='/favicon.ico' width={28} height={28} className="w-7 h-7 mb-8" alt='favicon' />
+          <p className="text-[#9ea3af] text-sm">{text}</p>
+          <div className="flex gap-4 items-center mt-8">
+            <Link href={'/'}><Image src='/assets/tiktok.svg' width={24} height={24} className="w-6 h-6" alt='tiktok' /></Link>
+            <Link href={'/'}><Image src='/assets/ins.svg' width={28} height={28} className="w-7 h-7" alt='ins' /></Link>
+            <Link href={'/'}><Image src='/assets/twtter.svg' width={24} height={24} className="w-6 h-6" alt='twtter' /></Link>
+            <Link href={'/'}><Image src='/assets/youtube.svg' width={28} height={28} className="w-7 h-7" alt='youtube' /></Link>
+          </div>
+        </div>
+
+        <div className="mt-16 px-2 flex flex-col gap-10 text-[#9ea3af] text-sm md:flex-row md:gap-3 xl:gap-28">
+          <div className="flex flex-col flex-1 gap-6">
+            <h3 className="font-semibold leading-6 text-gray-900">Products</h3>
+            {navsGroup.products.map((v, i) => <Link key={i} className="text-nowrap hover:text-gray-900" href={v.link}>{v.text}</Link>)}
+          </div>
+
+          <div className="flex flex-col flex-1 gap-6">
+            <h3 className="font-semibold leading-6 text-gray-900">We also built</h3>
+            {navsGroup.weAlsoBuilt.map((v, i) => <Link key={i} className="text-nowrap hover:text-gray-900" href={v.link}>{v.text}</Link>)}
+          </div>
+
+          <div className="flex flex-col flex-1 gap-6">
+            <h3 className="font-semibold leading-6 text-gray-900">Company</h3>
+            {navsGroup.company.map((v, i) => <Link key={i} className="text-nowrap hover:text-gray-900" href={v.link}>{v.text}</Link>)}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import dynamic from 'next/dynamic';
-import Header, { HeaderLogoLinkType, HeaderNavsLinkType } from './components/Header';
-import Footer from './components/Footer';
+import dynamic from 'next/dynamic'
+import Header, { HeaderLogoLinkType, HeaderNavsLinkType } from './components/Header'
+import Footer, { FooterLinksGroupType } from './components/Footer'
 
 const Pdf = dynamic(() => import('./components/Pdf'), {
   ssr: false
@@ -13,14 +13,46 @@ export default function Home() {
     {text: 'Pricing', link: '/'},
     {text: 'Chrome extension', link: '/'},
     {text: 'Use cases', link: '/'},
-    {text: 'Get started →', link: '/'},
+    {text: 'Get started →', link: '/'}
   ]
+
+  const footerText = 'Chat with any PDF: ask questions, get summaries, find information, and more.'
+
+  const footerNavsGroup: FooterLinksGroupType = {
+    products: [
+      {text: 'Use cases', link: '/'},
+      {text: 'Chrome extension', link: '/'},
+      {text: 'API docs', link: '/'},
+      {text: 'Pricing', link: '/'},
+      {text: 'Video tutorials', link: '/'},
+      {text: 'Resources', link: '/'},
+      {text: 'Blog', link: '/'},
+      {text: 'FAQ', link: '/'}
+    ],
+    weAlsoBuilt: [
+      {text: 'Resume AI Scanner', link: '/'},
+      {text: 'Invoice AI Scanner', link: '/'},
+      {text: 'AI Quiz Generator', link: '/'},
+      {text: 'QuickyAI', link: '/'},
+      {text: 'Docsium', link: '/'},
+      {text: 'PDF GPTs', link: '/'},
+      {text: 'PDF AI generator', link: '/'},
+      {text: 'Other PDF tools', link: '/'}
+    ],
+    company: [
+      {text: 'PDF.ai vs ChatPDF', link: '/'},
+      {text: 'PDF.ai vs Acrobat Reader', link: '/'},
+      {text: 'Legal', link: '/'},
+      {text: 'Affiliate program 💵', link: '/'},
+      {text: 'Investor', link: '/'}
+    ]
+  }
   
   return (
     <div>
       <Header logoLink={headerLogoLink} navsLink={headerNavsLink}/>
       <Pdf />
-      <Footer />
+      <Footer text={footerText} navsGroup={footerNavsGroup}/>
     </div>
   )
 }
